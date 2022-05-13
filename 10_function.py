137283bc18b64a8c2b62728d23c150eab447e4bd
waqasqasid <waqasqasid@gmail.com>
2022-05-13 20:54:48 +0500

    functions

--- 10_function.py
+++ 10_function.py
@@ -1,9 +1,3 @@
-# # print('we are learning python')
-# # print('we are learning python')
-# # print('we are learning python')
-
-# # defining a function
-
 
 def alnafi():
     x = "this is our platform"
@@ -12,3 +6,20 @@ def alnafi():
     print(x)
 
 print(alnafi())
+
+# 2
+def fun1():
+    print('waqas kaisa hain tu')
+
+fun1()
+
+# 3
+def fun2(age):
+    if age == 6:
+        print('Abdullah can join school')
+    elif age <6:
+        print('Abdullah is still baby')
+    elif age >6:
+        print('Abdullah can join higher school')
+fun2(6)
+
